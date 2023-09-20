dfa94724e5c4f09d236b4a55d457b189f6cab8c3
VINANK <neupaneayush3@gmail.com>
2023-09-20 10:36:41 +0545

    added forms and login system

--- musicPlayer/forms/forms.py
+++ musicPlayer/forms/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.forms import PasswordInput
 
 class LoginForm(forms.Form):
 	Username = forms.CharField(label="Username", max_length=15)
 	Email=forms.EmailField(label="Email")
-	Password=forms.CharField(label="Password")
+	Password=forms.CharField(label="Password",widget=PasswordInput())
--- musicPlayer/forms/views.py
+++ musicPlayer/forms/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .forms import LoginForm
 
 # Create your views here.
 def login(request):
-	form=LoginForm()
+	if request.method=='POST':
+		form=LoginForm(request.POST)
+		if form.is_valid():
+			data=form.cleaned_data
+			username=data['Username']
+			return render(request,'index.html',{'username':username})
+
+	else:
+		form=LoginForm()
 	return render(request,'login.html',{"form":form})
